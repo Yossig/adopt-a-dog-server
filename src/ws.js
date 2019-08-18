fff0022900ced0d76762226d646c365e3a6b13be
Yossig <yosigartner@gmail.com>
2019-08-18 18:10:49 +0300

    refactor the json sent to client from websocket, contains 'actions' and 'data' to identify which action to invoke in client side

--- src/ws.js
+++ src/ws.js
@@ -2,15 +2,13 @@ const WebScoket = require('ws');
 class webSocket {
 
   constructor() {
-    this.wss = new WebScoket.Server({port:3001})
+    this.wss = new WebScoket.Server({ port: 3001 })
     console.log("ws server running on port 3001")
   }
 
   broadcastDeletion(doc) {
-    console.log("broadcasting")
-    this.wss.clients.forEach(client=> {
-      console.log("broadcast to user", doc)
-      client.send(JSON.stringify(doc))
+    this.wss.clients.forEach(client => {
+      client.send(JSON.stringify({ action: 'dogRemoved', data: doc }))
     })
   }
 }
