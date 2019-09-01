2db75b98febf03072226b91064cf21001799d89c
Yossig <yosigartner@gmail.com>
2019-09-01 15:31:25 +0300

    fix location updating

--- src/user/user.service.js
+++ src/user/user.service.js
@@ -15,7 +15,14 @@ class userService {
     return newUser.save();
   }
 
-  update(user) {
+  async update(user) {
+    const uri = `https://maps.googleapis.com/maps/api/geocode/json?address=${user.address.country},${user.address.city},${user.address.street},${user.address.number}&key=AIzaSyCgfUTGzlzr4DmsTbCy4OBkxqZ8y7nimxI`
+    const { results } = await requestPromise({
+      uri: uri,
+      json: true
+    })
+
+    user.address.location = results[0].geometry.location
     return userModel.findByIdAndUpdate(user._id, user, { new: true }).exec();
   }
 }
